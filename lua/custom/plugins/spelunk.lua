de4aecedfea8496b838223b8dd6dc6f6901999e2
Androo Frowns <17219603+AndrooFrowns@users.noreply.github.com>
2025-07-31 05:07:27 -0400

    resolve double usage of require lazy

--- lua/custom/plugins/spelunk.lua
+++ lua/custom/plugins/spelunk.lua
@@ -1,17 +1,13 @@
 return {
-	require("lazy").setup({
-		{
-			"EvWilson/spelunk.nvim",
-			dependencies = {
-				"nvim-lua/plenary.nvim", -- For window drawing utilities
-				"nvim-telescope/telescope.nvim", -- Optional: for fuzzy search capabilities
-				"nvim-treesitter/nvim-treesitter", -- Optional: for showing grammar context
-			},
-			config = function()
-				require("spelunk").setup({
-					enable_persist = true,
-				})
-			end,
-		},
-	}),
+	"EvWilson/spelunk.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- For window drawing utilities
+		"nvim-telescope/telescope.nvim", -- Optional: for fuzzy search capabilities
+		"nvim-treesitter/nvim-treesitter", -- Optional: for showing grammar context
+	},
+	config = function()
+		require("spelunk").setup({
+			enable_persist = true,
+		})
+	end,
 }
