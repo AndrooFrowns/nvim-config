9dcd0a00e9fb22a71e6a4c75cf3d5161420d43e8
Androo Frowns <17219603+AndrooFrowns@users.noreply.github.com>
2025-07-31 04:49:47 -0400

    fix typo

--- lua/custom/plugins/spelunk.lua
+++ lua/custom/plugins/spelunk.lua
@@ -1,15 +1,17 @@
-require("lazy").setup({
-	{
-		"EvWilson/spelunk.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- For window drawing utilities
-			"nvim-telescope/telescope.nvim", -- Optional: for fuzzy search capabilities
-			"nvim-treesitter/nvim-treesitter", -- Optional: for showing grammar context
+return {
+	require("lazy").setup({
+		{
+			"EvWilson/spelunk.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim", -- For window drawing utilities
+				"nvim-telescope/telescope.nvim", -- Optional: for fuzzy search capabilities
+				"nvim-treesitter/nvim-treesitter", -- Optional: for showing grammar context
+			},
+			config = function()
+				require("spelunk").setup({
+					enable_persist = true,
+				})
+			end,
 		},
-		config = function()
-			require("spelunk").setup({
-				enable_persist = true,
-			})
-		end,
-	},
-})
+	}),
+}
